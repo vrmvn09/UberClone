74f63ec4fa3b757654eb84fadec9904ab0580dde
Arman <armanurstem@gmail.com>
2024-06-05 18:58:32 +0500

    UberClone create files

---
+++ UberClone/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,41 @@
+//
+//  UberMapViewRepresentable.swift
+//  UberClone
+//
+//  Created by Arman  Urstem on 05.06.2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapViewCoordinator {
+        return MapViewCoordinator(parent: self)
+    }
+}
+
+extension UberMapViewRepresentable {
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
