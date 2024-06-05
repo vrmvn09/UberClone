2c398db898b2236ef4b3dfcf8dc86b309da0e7aa
Arman <armanurstem@gmail.com>
2024-06-05 18:44:45 +0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Arman  Urstem on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
